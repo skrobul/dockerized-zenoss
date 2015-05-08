0b97747e0e74d0d4830fdf53761bee958b04795f
Marek Skrobacki <skrobul@skrobul.com>
2015-05-09 00:30:20 +0100

    set rabbitmq password during container start

--- start_zenoss.sh
+++ start_zenoss.sh
@@ -2,4 +2,19 @@
 for serv in snmpd memcached mysql exim rabbitmq-server zenoss crond jexec ; do
     service $serv start
 done
+# RabbitMQ ctl stores configuration that is specific to current hostname
+# which changes every time when the container is started, so we have to 
+# re-set the passwords first
+
+PASS="grep amqppassword \$ZENHOME/etc/global.conf | awk '{print \$2}'"
+RABBITMQCTL=$(which rabbitmqctl)
+$RABBITMQCTL stop_app
+$RABBITMQCTL reset
+$RABBITMQCTL start_app
+$RABBITMQCTL add_user "zenoss" "$(su -l zenoss -c "$PASS")"
+$RABBITMQCTL add_vhost "/zenoss"
+$RABBITMQCTL set_permissions -p "/zenoss" "zenoss" '.*' '.*' '.*'
+
+
+# Finally, redirect logs to stdout
 tail -f /opt/zenoss/log/*.log
